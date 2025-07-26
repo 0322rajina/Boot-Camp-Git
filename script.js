73a967becd9df98ae1f49c34db8db6c8caeb9c94
rajinagurung <grgrajina00@gamil.com>
2025-07-26 12:36:14 +0545

    show name

---
+++ script.js
@@ -0,0 +1,5 @@
+const name={id:1,name:"John Doe",age:30,city:"New York"};
+const greet = (person) => {
+    return `Hello, my name is ${person.name} and I am ${person.age} years old.`;
+}
+console.log(greet(name));
